fdfe240f22ce7f8ae3c0bb4fc7db210540e2a993
Everton Soares Rodrigues <everton_rodrigues@virgo.atlantico.net.br>
2019-10-11 17:49:24 -0300

    Adding vscode extensions

---
+++ my_vscode_extensions.sh
@@ -0,0 +1,12 @@
+code --install-extension GabrielBB.vscode-lombok
+code --install-extension naco-siren.gradle-language
+code --install-extension Pivotal.vscode-spring-boot
+code --install-extension redhat.java
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+code --install-extension vscjava.vscode-spring-boot-dashboard
+code --install-extension vscjava.vscode-spring-initializr
