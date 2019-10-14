819e0bfdd96225a920211ffeba9356ad6ce2154e
Everton Soares Rodrigues <everton_rodrigues@virgo.atlantico.net.br>
2019-10-14 10:25:01 -0300

    Adding routes in a ym approach

--- edge-service/src/main/java/com/cajalab/edgeservice/EdgeServiceApplication.java
+++ edge-service/src/main/java/com/cajalab/edgeservice/EdgeServiceApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+// import org.springframework.cloud.gateway.route.RouteLocator;
+// import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -22,15 +22,15 @@ public class EdgeServiceApplication {
 		return new DiscoveryClientRouteDefinitionLocator(discoveryClient, properties);
 	}
 
-	@Bean
-	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-		//@formatter:off
-		return builder.routes()
-				// customers
-				.route("customers-find-all", r -> r.path("/customers")
-					.uri("lb://customer-service/customers"))
-				.build();
-		//@formatter:on
-	}
+	// @Bean
+	// public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+	// 	//@formatter:off
+	// 	return builder.routes()
+	// 			// load balance customers
+	// 			.route("customers-find-all", r -> r.path("/customers")
+	// 				.uri("lb://customer-service/customers"))				
+	// 			.build();
+	// 	//@formatter:on
+	// }
 
 }
