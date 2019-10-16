039cb9050e70c5a98fe22e6374b8432ee787f78f
Everton Soares Rodrigues <everton_rodrigues@virgo.atlantico.net.br>
2019-10-16 13:22:36 -0300

    Renaming classes

--- consumer-service/src/main/java/com/cajalab/consumerservice/ConsumerServiceApplication.java
+++ consumer-service/src/main/java/com/cajalab/consumerservice/ConsumerServiceApplication.java
@@ -8,10 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class CustomerServiceApplication {
+public class ConsumerServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerServiceApplication.class, args);
+		SpringApplication.run(ConsumerServiceApplication.class, args);
 	}
 
 	@Bean
--- consumer-service/src/test/java/com/cajalab/customerservice/ConsumerServiceApplicationTests.java
+++ consumer-service/src/test/java/com/cajalab/customerservice/ConsumerServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CustomerServiceApplicationTests {
+public class ConsumerServiceApplicationTests {
 
 	@Test
 	public void contextLoads() {
